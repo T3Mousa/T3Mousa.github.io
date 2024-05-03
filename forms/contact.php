e67c2d4833ae2c587b301239b45571a0063a4766
Tamara <jermanust88@gmail.com>
2024-05-03 17:13:30 -0400

    added receiving email address

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'jermanust88@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -17,7 +17,7 @@
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
+
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
@@ -31,7 +31,7 @@
     'password' => 'pass',
     'port' => '587'
   );
-  */
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
